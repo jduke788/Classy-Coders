5a1ff276c4892f1cfe73b581710473230366c39e
Michael Widjaya <michaeladhipramanaw@gmail.com>
2024-10-02 12:50:31 +0100

    Sales and SWE

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,6 +1,19 @@
-class SoftwareEngineer {
+const Employee = require("./Employee")
 
+class SoftwareEngineer extends Employee {
+    #programmingLanguages
+    constructor (name, position, salary, programmingLanguages) {
+        super(name, position, salary);
+        this.#programmingLanguages = programmingLanguages
+    }
 
+    getProgrammingLanguages() {
+        return this.#programmingLanguages
+    }
+
+    setProgrammingLanguage(language) {
+        this.#programmingLanguages.push(language)
+    }
 
     
 }
