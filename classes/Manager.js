f8c6e165fd2172be9a5cab66b1a5ac466077f922
jduke788 <jduke788@gmail.com>
2024-10-02 12:49:35 +0100

    one test fail on employee, manager started

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,18 @@
-class Manager {
+//const Employee = require('./Employee')
+//class Manager extends Employee {
+    //#employeesManaged 
+    //constructor(name, position, salary, isHired, department, employeesManaged) {
+        //super(name, position, salary, isHired)
+        //this.department = department
+        //this.#employeesManaged = employeesManaged
+    //}
+
 
 
 
     
-}
+//}
 
-module.exports = {
-    Manager,
-}
+//module.exports = {
+    //Manager,
+//}
